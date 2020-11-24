70ccb7d6295e023d926272feecfcf036c5ec1d8f
naren <naranrana02@gmail.com>
2020-11-24 14:50:45 +0800

    Kubs integration updated

--- build.sh
+++ build.sh
@@ -13,6 +13,8 @@ docker push narenrana02/cnm-retail_main_app
 docker push narenrana02/cnm-retail_cnm_payment
 docker push narenrana02/cnm-retail_cnm_server
 
+kubectl apply -f  ./deployment-init.yaml
+
 #kubectl apply -f  ./deployment-cnm-server.yaml
 kubectl apply -f  ./cnm-auth/deployment.yaml
 kubectl apply -f  ./cnm-carts/deployment.yaml
@@ -30,28 +32,28 @@ kubectl apply -f  ./payment-app/deployment.yaml
 kubectl apply -f  ./deployment-ingress.yaml
 
 
-kubectl describe namespace cnm-auth
+
 kubectl describe service cnm-auth
 kubectl describe deployment cnm-auth
 
-kubectl describe namespace cnm-carts
+
 kubectl describe service cnm-cats
 kubectl describe deployment cnm-carts
 
-kubectl describe namespace cnm-coupons
+
 kubectl describe service cnm-coupons
 kubectl describe deployment cnm-coupons
 
 
-kubectl describe namespace cnm-offers
+
 kubectl describe service cnm-offers
 kubectl describe deployment cnm-offers
 
-kubectl describe namespace cnm-orders
+
 kubectl describe service cnm-orders
 kubectl describe deployment cnm-orders
 
-kubectl describe namespace cnm-payments
+
 kubectl describe service cnm-payments
 kubectl describe deployment cnm-payments
 
@@ -59,16 +61,15 @@ kubectl describe namespace cnm-products
 kubectl describe service cnm-products
 kubectl describe deployment cnm-products
 
-kubectl describe namespace cnm-users
 kubectl describe service cnm-users
 kubectl describe deployment cnm-users
 
 
-kubectl describe namespace main-app
+
 kubectl describe service main-app
 kubectl describe deployment main-app
 
-kubectl describe namespace payment-app
+
 kubectl describe service payment-app
 kubectl describe deployment payment-app
 
--- clean.sh
+++ clean.sh
@@ -1,37 +1,36 @@
 #docker-compose  build
 
-
+kubectl delete namespace app
 #kubectl delete namespace cnm-server
-kubectl delete namespace cnm-auth
 kubectl delete service cnm-auth
 kubectl delete deployment cnm-auth
 
-kubectl delete namespace cnm-carts
+
 kubectl delete service cnm-cats
 kubectl delete deployment cnm-carts
 
-kubectl delete namespace cnm-coupons
+
 kubectl delete service cnm-coupons
 kubectl delete deployment cnm-coupons
 
 
-kubectl delete namespace cnm-offers
+
 kubectl delete service cnm-offers
 kubectl delete deployment cnm-offers
 
-kubectl delete namespace cnm-orders
+
 kubectl delete service cnm-orders
 kubectl delete deployment cnm-orders
 
-kubectl delete namespace cnm-payments
+
 kubectl delete service cnm-payments
 kubectl delete deployment cnm-payments
 
-kubectl delete namespace cnm-products
+
 kubectl delete service cnm-products
 kubectl delete deployment cnm-products
 
-kubectl delete namespace cnm-users
+
 kubectl delete service cnm-users
 kubectl delete deployment cnm-users
 
