9b857d2f4484d76669634445c8e97c21299fd4e5
naren <>
2021-02-11 02:20:24 +0800

    bug fix

--- build.sh
+++ build.sh
@@ -1,78 +1,89 @@
 sh ./clean.sh
 
+echo "Build docker images cnm-retail_main_app, cnm-retail_cnm_payment, cnm-retail_cnm_server"
 docker-compose  build
 
-
+echo "Tag docker images cnm-retail_main_app, cnm-retail_cnm_payment, cnm-retail_cnm_server"
 docker tag cnm-retail_main_app narenrana02/cnm-retail_main_app
 docker tag cnm-retail_cnm_payment narenrana02/cnm-retail_cnm_payment
-#Reuse same image for all microservices
 docker tag cnm-retail_cnm_server narenrana02/cnm-retail_cnm_server
 
-
+echo "Pushing docker images cnm-retail_main_app, cnm-retail_cnm_payment, cnm-retail_cnm_server to docker repository"
+echo "Warning - Use local or custom remote repository"
 docker push narenrana02/cnm-retail_main_app
 docker push narenrana02/cnm-retail_cnm_payment
 docker push narenrana02/cnm-retail_cnm_server
 
+echo "Creating services and deployments"
+echo "Step-1 - init"
 kubectl apply -f  ./deployment-init.yaml
-
-#kubectl apply -f  ./deployment-cnm-server.yaml
+echo "Step-2 - cnm-auth"
 kubectl apply -f  ./cnm-auth/deployment.yaml
+echo "Step-3 - cnm-carts"
 kubectl apply -f  ./cnm-carts/deployment.yaml
+echo "Step-4 - cnm-coupons"
 kubectl apply -f  ./cnm-coupons/deployment.yaml
+echo "Step-5 - cnm-offers"
 kubectl apply -f  ./cnm-offers/deployment.yaml
+echo "Step-6 - cnm-orders"
 kubectl apply -f  ./cnm-orders/deployment.yaml
+echo "Step-7 - cnm-payments"
 kubectl apply -f  ./cnm-payments/deployment.yaml
+echo "Step-8 - cnm-products"
 kubectl apply -f  ./cnm-products/deployment.yaml
+echo "Step-9 - cnm-users"
 kubectl apply -f  ./cnm-users/deployment.yaml
 #Main Ui App
+echo "Step-10 - cnm-app"
 kubectl apply -f  ./cnm-app/deployment.yaml
 #Main payment App
+echo "Step-11 - payment-app"
 kubectl apply -f  ./payment-app/deployment.yaml
 #Router
+echo "Step-12 - deployment-ingress"
 kubectl apply -f  ./deployment-ingress.yaml
 
+echo "Describe services and deployments"
+kubectl describe service cnm-auth -n app
+kubectl describe deployment cnm-auth -n app
 
 
-kubectl describe service cnm-auth
-kubectl describe deployment cnm-auth
+kubectl describe service cnm-cats -n app
+kubectl describe deployment cnm-carts -n app
 
 
-kubectl describe service cnm-cats
-kubectl describe deployment cnm-carts
-
-
-kubectl describe service cnm-coupons
-kubectl describe deployment cnm-coupons
-
-
-
-kubectl describe service cnm-offers
-kubectl describe deployment cnm-offers
-
-
-kubectl describe service cnm-orders
-kubectl describe deployment cnm-orders
-
-
-kubectl describe service cnm-payments
-kubectl describe deployment cnm-payments
-
-kubectl describe namespace cnm-products
-kubectl describe service cnm-products
-kubectl describe deployment cnm-products
-
-kubectl describe service cnm-users
-kubectl describe deployment cnm-users
+kubectl describe service cnm-coupons -n app
+kubectl describe deployment cnm-coupons -n app
 
 
 
-kubectl describe service main-app
-kubectl describe deployment main-app
+kubectl describe service cnm-offers -n app
+kubectl describe deployment cnm-offers -n app
 
 
-kubectl describe service payment-app
-kubectl describe deployment payment-app
 
-kubectl describe ingress cnm-ingress
+kubectl describe service cnm-orders -n app
+kubectl describe deployment cnm-orders -n app
+
+
+kubectl describe service cnm-payments -n app
+kubectl describe deployment cnm-payments -n app
+
+kubectl describe service cnm-products -n app
+kubectl describe deployment cnm-products -n app
+
+kubectl describe service cnm-users -n app
+kubectl describe deployment cnm-users -n app
+
+
+
+kubectl describe service main-app -n app
+kubectl describe deployment main-app -n app
+
+
+kubectl describe service payment-app -n app
+kubectl describe deployment payment-app -n app
+
+kubectl describe ingress cnm-ingress -n app
 
 
--- clean.sh
+++ clean.sh
@@ -1,46 +1,48 @@
 #docker-compose  build
 
+echo "Deleting cnm-auth services and deployments"
+kubectl delete deployment cnm-auth -n app
+kubectl delete service cnm-auth -n app
+
+echo "Deleting cnm-carts services and deployments"
+kubectl delete deployment cnm-carts -n app
+kubectl delete service cnm-cats -n app
+
+echo "Deleting cnm-coupons services and deployments"
+kubectl delete deployment cnm-coupons -n app
+kubectl delete service cnm-coupons -n app
+
+echo "Deleting cnm-offers services and deployments"
+kubectl delete deployment cnm-offers -n app
+kubectl delete service cnm-offers -n app
+
+echo "Deleting cnm-orders services and deployments"
+kubectl delete deployment cnm-orders -n app
+kubectl delete service cnm-orders -n app
+
+echo "Deleting cnm-payments services and deployments"
+kubectl delete deployment cnm-payments -n app
+kubectl delete service cnm-payments -n app
+
+echo "Deleting cnm-products services and deployments"
+kubectl delete deployment cnm-products -n app
+kubectl delete service cnm-products -n app
+
+echo "Deleting cnm-users services and deployments"
+kubectl delete deployment cnm-users -n app
+kubectl delete service cnm-users -n app
+
+echo "Deleting main-app services and deployments"
+kubectl delete deployment main-app -n app
+kubectl delete service main-app -n app
+
+echo "Deleting payment-app services and deployments"
+kubectl delete deployment payment-app -n app
+kubectl delete service payment-app -n app
+
+echo "Deleting cnm-ingress services and deployments"
+kubectl delete ingress cnm-ingress -n app
+
+echo "Deleting app services and deployments"
 kubectl delete namespace app
-#kubectl delete namespace cnm-server
-kubectl delete service cnm-auth
-kubectl delete deployment cnm-auth
-
-
-kubectl delete service cnm-cats
-kubectl delete deployment cnm-carts
-
-
-kubectl delete service cnm-coupons
-kubectl delete deployment cnm-coupons
-
-
-
-kubectl delete service cnm-offers
-kubectl delete deployment cnm-offers
-
-
-kubectl delete service cnm-orders
-kubectl delete deployment cnm-orders
-
-
-kubectl delete service cnm-payments
-kubectl delete deployment cnm-payments
-
-
-kubectl delete service cnm-products
-kubectl delete deployment cnm-products
-
-
-kubectl delete service cnm-users
-kubectl delete deployment cnm-users
-
-#kubectl delete namespace main-app
-kubectl delete service main-app
-kubectl delete deployment main-app
-
-#kubectl delete namespace payment-app
-kubectl delete service payment-app
-kubectl delete deployment payment-app
-
-kubectl delete ingress cnm-ingress
 
